ab55181004e4faf1010c3e5436d26a476a56a22d
AsakaJX <yourasaka@protonmail.com>
2025-07-11 21:27:58 +0300

    Add Result (+ after result) handling

--- main.js
+++ main.js
@@ -1,24 +1,24 @@
-function add(a, b) {
-  return a + b;
+function sum(a, b) {
+  return +a + +b;
 }
 
 function subtract(a, b) {
-  return a - b;
+  return +a - +b;
 }
 
 function multiply(a, b) {
-  return a * b;
+  return +a * +b;
 }
 
 function divide(a, b) {
-  return a / b;
+  return +a / +b;
 }
 
 const display = document.querySelector('.display');
 const calculator = document.querySelector('.calculator');
 calculator.addEventListener('click', tokenize);
 
-const tokens = [];
+let tokens = [];
 const operations = '/*-+=';
 
 function tokenize(event) {
@@ -26,10 +26,15 @@ function tokenize(event) {
     return;
   }
   const token = event.target.textContent;
-  console.log('pressed: ' + token);
 
   if (operations.includes(tokens.at(-1)) && operations.includes(token)) {
     throw Error("you can't use two operations in a row!");
+  }
+
+  if (result !== null) {
+    result = null;
+    tokens = [];
+    return;
   }
 
   const isOperatorExist =
@@ -49,23 +54,80 @@ function tokenize(event) {
     if (!isOperatorExist && token === '=') {
       throw Error("there' nothing to evaluate at this point!");
     }
-    tokens.push(token);
+    if (token !== '=') {
+      tokens.push(token);
+    }
   } else {
     tokens[tokens.length - 1] += token;
   }
 
+  console.log(tokens);
+
   handleDisplay();
 }
 
-function handleDisplay() {
-  console.log(tokens);
+function handleDisplay(result) {
+  display.textContent = tokens.toString().replaceAll(',', ' ') + '_';
 
-  if (!tokens.includes('=')) {
-    display.textContent = tokens.toString().replaceAll(',', ' ') + '_';
+  if (result) {
+    display.textContent = result;
   }
 }
 
 const equalButton = document.querySelector('#equal');
-equalButton.addEventListener('click', handleEvaluation());
+equalButton.addEventListener('click', handleEquation);
 
-function handleEvaluation() {}
+let result = null;
+
+function handleEquation() {
+  let equation = [...tokens];
+  const pattern = /[^\w\s]/g;
+  const indexesOfOperators = [...equation.join('').matchAll(pattern)];
+  const equationObject = {};
+
+  indexesOfOperators.forEach((item) => {
+    const operatorIndex = item.index;
+
+    equationObject[operatorIndex] = {};
+    equationObject[operatorIndex] = {
+      operator: equation[1],
+      left: equation[0],
+      right: equation[2],
+    };
+
+    const operator = equationObject[operatorIndex].operator;
+
+    const leftToken = equationObject[operatorIndex].left;
+    const rightToken = equationObject[operatorIndex].right;
+
+    let innerResult = null;
+    switch (operator) {
+      case '+':
+        innerResult = sum(leftToken, rightToken);
+        break;
+
+      case '-':
+        innerResult = subtract(leftToken, rightToken);
+        break;
+
+      case '*':
+        innerResult = multiply(leftToken, rightToken);
+        break;
+
+      case '/':
+        innerResult = divide(leftToken, rightToken);
+        break;
+
+      default:
+        throw Error(
+          'got an unexpected token during handling evaluation process'
+        );
+    }
+
+    equation = equation.slice(3);
+    equation.unshift(innerResult);
+  });
+  result = equation[0];
+  tokens = [...equation];
+  handleDisplay(result);
+}
